7c8f4e58495cd1bde0c476c965b2debec0cf30b3
zidan4 <zidanjaffar@gmail.com>
2025-02-20 17:07:23 +0300

    react

---
+++ app.jsx
@@ -0,0 +1,11 @@
+import Header form './components/Header.jsx';
+import Main form   './components/Main.jsx';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Main />
+        </div>
+    )
+}
---
+++ index.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client';
+import { createSignal } from 'solid-js';
+
+import App from './App';
+
+createRoot(document.getElementById('root')).render(App);
